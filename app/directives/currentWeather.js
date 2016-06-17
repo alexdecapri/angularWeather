e7c6bddae4ff731969d26fae978405af3fad2b18
philippschulte <philipp.schulte@ymail.com>
2016-06-17 17:53:41 -0600

    Complete 2.Step instructions

--- app/directives/currentWeather.js
+++ app/directives/currentWeather.js
@@ -1,5 +1,43 @@
-app.directive('currentWeather', function() {
+app.directive('currentWeatherForecast', function() {
 
-  return {};
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/templates/currentWeather.html',
+    replace: false,
+    controller: function($scope, locationService, weatherService) {
+
+      init();
+
+      function init() {
+        updatePropsForCurrentLocation(false, false, false, {}, '');
+        $scope.weatherSummery = weatherService.weatherSummery;
+        getWeatherForCurrentLocation();
+      }
+
+      function getWeatherForCurrentLocation() {
+        $scope.loadingForCurrentLocation = true;
+
+        locationService.getLocation()
+          .then(function(city) {
+            return weatherService.getWeather(city);
+          })
+          .then(function(weatherForecastForCurrentLocation) {
+            updatePropsForCurrentLocation(false, false, true, weatherForecastForCurrentLocation, '');
+          })
+          .catch(function(err) {
+            updatePropsForCurrentLocation(false, true, false, {}, err.message);
+          });
+      }
+
+      function updatePropsForCurrentLocation(loadingForCurrentLocation, isErrorForCurrentLocation, isWeatherForCurrentLocation, weatherForecastForCurrentLocation, errorMessageForCurrentLocation) {
+        $scope.loadingForCurrentLocation = loadingForCurrentLocation;
+        $scope.isErrorForCurrentLocation = isErrorForCurrentLocation;
+        $scope.isWeatherForCurrentLocation = isWeatherForCurrentLocation;
+        $scope.weatherForecastForCurrentLocation = weatherForecastForCurrentLocation;
+        $scope.errorMessageForCurrentLocation = errorMessageForCurrentLocation;
+      }
+
+    }
+  };
 
 });
